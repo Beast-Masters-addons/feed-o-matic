1fbc1cfaa38ab82f588fc9e8af2d0c01137ddae6
datagutten <datagutten@datagutten.net>
2022-04-02 11:42:47 +0200

    Add test for parsing food name from feeding emote

---
+++ tests/wow_constants.lua
@@ -0,0 +1,10 @@
+_G['FEEDPET_LOG_FIRSTPERSON'] = 'Your pet begins eating the %s.'
+_G.QuestDifficultyColors = {
+    ["impossible"]		= { r = 1.00, g = 0.10, b = 0.10, font = "QuestDifficulty_Impossible" };
+    ["verydifficult"]	= { r = 1.00, g = 0.50, b = 0.25, font = "QuestDifficulty_VeryDifficult" };
+    ["difficult"]		= { r = 1.00, g = 0.82, b = 0.00, font = "QuestDifficulty_Difficult" };
+    ["standard"]		= { r = 0.25, g = 0.75, b = 0.25, font = "QuestDifficulty_Standard" };
+    ["trivial"]			= { r = 0.50, g = 0.50, b = 0.50, font = "QuestDifficulty_Trivial" };
+    ["header"]			= { r = 0.70, g = 0.70, b = 0.70, font = "QuestDifficulty_Header" };
+    ["disabled"]		= { r = 0.498, g = 0.498, b = 0.498, font = "QuestDifficulty_Impossible" };
+};
