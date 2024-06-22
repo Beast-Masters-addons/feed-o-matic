8f623166a7035d4ca297dcb35773011736915d3f
datagutten <datagutten@datagutten.net>
2024-06-22 19:58:29 +0200

    Add test for food log

---
+++ tests/test_food.lua
@@ -0,0 +1,26 @@
+local lu = require('luaunit')
+
+loadfile('build_utils/utils/load_toc.lua')('./test.toc')
+loadfile('build_utils/utils/load_toc.lua')('../GFW_FeedOMatic.toc')
+local addon = _G.GFW_FeedOMatic
+---@type FOM_PetInfo
+local info = addon:GetModule('FOM_PetInfo')
+---@type FOM_FoodLogger
+local foodLog = addon:GetModule("FOM_FoodLogger")
+
+_G['test'] = {}
+local test = _G['test']
+
+function test:testSaveFood()
+    local message = 'Your pet begins eating the Tiger Meat.'
+    local item = foodLog.get('Bear', 12202)
+    lu.assertNil(item)
+    info:OnEnable()
+    info.petFamily = "Bear"
+    info.petLevel = 70
+    info:CHAT_MSG_PET_INFO('CHAT_MSG_PET_INFO', message)
+    local item2 = foodLog.get('Bear', 12202)
+    lu.assertTrue(foodLog.is_good(12202, 'Bear'))
+end
+
+os.exit(lu.LuaUnit.run())
--- tests/wow_constants.lua
+++ tests/wow_constants.lua
@@ -1,10 +1,31 @@
 _G['FEEDPET_LOG_FIRSTPERSON'] = 'Your pet begins eating the %s.'
 _G.QuestDifficultyColors = {
-    ["impossible"]		= { r = 1.00, g = 0.10, b = 0.10, font = "QuestDifficulty_Impossible" };
-    ["verydifficult"]	= { r = 1.00, g = 0.50, b = 0.25, font = "QuestDifficulty_VeryDifficult" };
-    ["difficult"]		= { r = 1.00, g = 0.82, b = 0.00, font = "QuestDifficulty_Difficult" };
-    ["standard"]		= { r = 0.25, g = 0.75, b = 0.25, font = "QuestDifficulty_Standard" };
-    ["trivial"]			= { r = 0.50, g = 0.50, b = 0.50, font = "QuestDifficulty_Trivial" };
-    ["header"]			= { r = 0.70, g = 0.70, b = 0.70, font = "QuestDifficulty_Header" };
-    ["disabled"]		= { r = 0.498, g = 0.498, b = 0.498, font = "QuestDifficulty_Impossible" };
+    ["impossible"] = { r = 1.00, g = 0.10, b = 0.10, font = "QuestDifficulty_Impossible" };
+    ["verydifficult"] = { r = 1.00, g = 0.50, b = 0.25, font = "QuestDifficulty_VeryDifficult" };
+    ["difficult"] = { r = 1.00, g = 0.82, b = 0.00, font = "QuestDifficulty_Difficult" };
+    ["standard"] = { r = 0.25, g = 0.75, b = 0.25, font = "QuestDifficulty_Standard" };
+    ["trivial"] = { r = 0.50, g = 0.50, b = 0.50, font = "QuestDifficulty_Trivial" };
+    ["header"] = { r = 0.70, g = 0.70, b = 0.70, font = "QuestDifficulty_Header" };
+    ["disabled"] = { r = 0.498, g = 0.498, b = 0.498, font = "QuestDifficulty_Impossible" };
 };
+
+_G.C_AddOns = {}
+function C_AddOns.GetAddOnMetadata(name, variable)
+    local variables = {
+        Title = "Fizzwidget Feed-O-Matic",
+        Version = "1.0",
+    }
+    return variables[variable]
+
+end
+
+GetAddOnMetadata = C_AddOns.GetAddOnMetadata
+_G.SPELLDISMISSPETSELF = "Your %s is dismissed."
+
+function GetItemInfo(item)
+    print('itemInf', item)
+    if item == "Tiger Meat" then
+        return "Tiger Meat", "|cffffffff|Hitem:12202::::::::80:::::|h[Tiger Meat]|h|r\");", 1, 30, 0
+    end
+end
+
