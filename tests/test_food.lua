11dfa5d39c46d8779e2497ca223513cb320ee344
Anders Birkenes <datagutten@datagutten.net>
2025-08-21 16:45:33 +0200

    Add test for food.isKnownFood

--- tests/test_food.lua
+++ tests/test_food.lua
@@ -1,12 +1,14 @@
 local lu = require('luaunit')
 
 loadfile('build_utils/utils/load_toc.lua')('./test.toc')
-loadfile('build_utils/utils/load_toc.lua')('../GFW_FeedOMatic.toc', { 'CurrentProfession.lua', 'LibProfessions.lua' })
+loadfile('build_utils/utils/load_toc.lua')('../GFW_FeedOMatic.toc')
 local addon = _G.GFW_FeedOMatic
 ---@type FOM_PetInfo
 local info = addon:GetModule('FOM_PetInfo')
 ---@type FOM_FoodLogger
 local foodLog = addon:GetModule("FOM_FoodLogger")
+---@type FOM_Food
+local food = addon:GetModule("FOM_Food")
 
 _G['test'] = {}
 local test = _G['test']
@@ -21,6 +23,12 @@ function test:testSaveFood()
     info:CHAT_MSG_PET_INFO('CHAT_MSG_PET_INFO', message)
     local item2 = foodLog.get('Bear', 12202)
     lu.assertTrue(foodLog.is_good(12202, 'Bear'))
+end
+
+function test:testIsKnownFood()
+    lu.assertEquals(food.isKnownFood(12202), "Meat")
+    lu.assertEquals(food.isKnownFood(6889), "Fish")
+    lu.assertNil(food.isKnownFood(99999))
 end
 
 os.exit(lu.LuaUnit.run())
